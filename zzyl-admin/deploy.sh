87110710080e4fe021b466c4e388fcd44393d8a3
bot975 <botingliu2@gmail.com>
2026-09-15 01:27:10 +1000

    通过环境变量向容器传递OSS凭据

--- zzyl-admin/deploy.sh
+++ zzyl-admin/deploy.sh
@@ -25,5 +25,5 @@ fi
 # 启动容器
 echo "启动容器 $container_name"
 if [ $container_name = "zzyl-admin" ]; then
-    docker run -d --restart=always --name $container_name -v /usr/local/zzyl-admin/logs:/home/ruoyi/logs -p 9000:9000 $image_name:$image_tag
+    docker run -d --restart=always --name $container_name -e OSS_ACCESS_KEY_ID -e OSS_ACCESS_KEY_SECRET -v /usr/local/zzyl-admin/logs:/home/ruoyi/logs -p 9000:9000 $image_name:$image_tag
 fi
